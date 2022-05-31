9d75e686149378357542d75e519b9831fdc62554
Leo Fang <leo80042@gmail.com>
2022-05-31 16:39:40 -0400

    update build script for cross compilation

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,5 +1,10 @@
-
+if [[ $CONDA_BUILD_CROSS_COMPILATION == "1" ]]; then
+    # Get an updated config.sub and config.guess
+    cp $BUILD_PREFIX/share/gnuconfig/config.* .
+fi
 ./configure --prefix=$PREFIX
 make -j $CPU_COUNT
-make check
+if [[ $CONDA_BUILD_CROSS_COMPILATION != "1" ]]; then
+    make check
+fi
 make install
